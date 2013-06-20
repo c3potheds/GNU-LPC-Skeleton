46d777eae5b835328a16750ec2ec929b49cd4eaa
Rohit507 <Rohit507@gmail.com>
2013-06-20 11:34:10 -0400

    Removed unneccesary includes and useless code. This is much more barebones a place from which to start.

--- src/main.c
+++ src/main.c
@@ -1,10 +1,10 @@
 /*
- ===============================================================================
+ ==============================================
  Name        : main.c
  Author      :
  Version     :
  Description : main definition
- ===============================================================================
+ ==============================================
  */
 
 #ifdef __USE_CMSIS
@@ -15,7 +15,6 @@
 #include <NXP/crp.h>
 
 #include "main.h"
-#include "UMDLPC/system/clocking.h"
 
 // Variable to store CRP value in. Will be placed automatically
 // by the linker when "Enable Code Read Protect" selected.
@@ -23,28 +22,8 @@
 __CRP const unsigned int CRP_WORD = CRP_NO_CRP;
 
 int main(void) {
-  // Select 12MHz crystal oscillator
-  LPC_SC ->CLKSRCSEL = 1;
 
-  // Bypass PLL 0
-  PLL_bypass();
-
-  // Peripheral power
-  // LPC_SC->PCONP = 0;
-
-  // Peripheral clocks
-  // LPC_SC->PCLKSEL0 = 0;
-  // LPC_SC->PCLKSEL1 = 0;
-
-  // Setup GPIO pins
-  // LPC_GPIO0->FIODIR = 0;
-  // LPC_GPIO0->FIOSET = 0;
-
-  // Select pin modes
-  // LPC_PINCON->PINSEL1 = 0;
-
-  while (1) {
-
-  }
+  while (1);
   return 0;
+
 }
